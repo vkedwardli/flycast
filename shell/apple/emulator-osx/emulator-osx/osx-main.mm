b159d2abba8ab2e10e25fa9d22a9481baddc667b
Edward Li <vk.edward.li@gmail.com>
2026-02-04 21:18:03 +0800

    macOS: Do not resolve IP to hostnames for slow/misconfigured DNS

--- shell/apple/emulator-osx/emulator-osx/osx-main.mm
+++ shell/apple/emulator-osx/emulator-osx/osx-main.mm
@@ -243,7 +243,7 @@ NSString* runCommand(NSString* commandToRun) {
 }
 
 std::string os_GetConnectionMedium() {
-    NSString* bestInterface = runCommand(@"route get 8.8.8.8 | grep interface | awk '{split($0,a,\": \"); printf \"%s\", a[2]}'");
+    NSString* bestInterface = runCommand(@"route -n get 8.8.8.8 | grep interface | awk '{split($0,a,\": \"); printf \"%s\", a[2]}'");
 
 	if ( bestInterface == nil || [bestInterface isEqual:[NSNull null]] || ([bestInterface respondsToSelector:@selector(length)] && [bestInterface length] == 0)) {
 		return "Unknown";
