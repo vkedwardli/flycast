cad49b3a6841be8ffe226ecfceccc28de8501883
Shingo INADA <mecha.is06@gmail.com>
2026-02-22 17:31:38 +0900

    Fix build error on macos

--- core/ui/gui.cpp
+++ core/ui/gui.cpp
@@ -355,7 +355,7 @@ void gui_initFonts()
 #elif defined(__APPLE__) && !defined(TARGET_IPHONE)
     std::string fontDir = std::string("/System/Library/Fonts/");
     // Always load ShiftJIS for Gdxsv
-    io.Fonts->AddFontFromFileTTF((fontDir + "ヒラギノ角ゴシック W4.ttc").c_str(), fontSize, &font_cfg, cjk_ranges.Data);
+    io.Fonts->AddFontFromFileTTF((fontDir + "ヒラギノ角ゴシック W4.ttc").c_str(), fontSize, &fontConfig, cjk_ranges.Data);
 /* gdxsv: Load ShiftJIS only to prevent crash with older GPU
     extern std::string os_Locale();
     std::string locale = os_Locale();
